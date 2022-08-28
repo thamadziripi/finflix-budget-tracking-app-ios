f30b8e49bf8e047c1abdae0649e8bc6b1736541e
Tino Hamadziripi <hamadziripi.tino@gmail.com>
2022-08-29 00:59:47 +0100

    Added preset Finflix project files

---
+++ Finflix Financial Tracker/Finflix Financial Tracker/Finflix_Financial_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finflix_Financial_TrackerApp.swift
+//  Finflix Financial Tracker
+//
+//  Created by Tino Hamadziripi on 29/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Finflix_Financial_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
